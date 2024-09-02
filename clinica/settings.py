aad375e2a6c05f96390147d443279ecfa10be576
Sara Hermosa <shermosa@roshka.com>
2024-09-02 00:29:34 -0400

    whitenoise

--- clinica/settings.py
+++ clinica/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE="whitenoise.storage.CompressedStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
